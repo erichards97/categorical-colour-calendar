de0bbbe6cf9eef66ac7165b989243d9baee076cf
Edward <edwardorichards@gmail.com>
2021-02-14 13:47:47 +0000

    0.0.5 released - bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='categorical-colour-calendar',
-    version='0.0.5',
+    version='0.0.6',
     description='Draw monthly calendars and highlight dates',
     py_modules=['cccalendar'],
     package_dir={'': 'src'},
