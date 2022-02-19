dd6254088af1457ef6179eb5890dd51944a5c760
Edward Richards <edwardorichards@gmail.com>
2022-02-19 18:07:14 +0000

    Fix test resource loading on py37 and py38

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,4 +1,7 @@
-import importlib.resources as resources
+try:
+    from importlib.resources import files
+except ImportError:
+    from importlib_resources import files
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -18,5 +21,5 @@ def test_simple_calendar():
         actual = Path(tmpdir) / "simple.png"
         plt.savefig(actual, bbox_inches='tight')
 
-        expectation_image = resources.files(__package__) / "baseline_images" / "test_regression" / "simple.png"
+        expectation_image = files(__package__) / "baseline_images" / "test_regression" / "simple.png"
         compare.compare_images(expectation_image, actual, tol=0)
